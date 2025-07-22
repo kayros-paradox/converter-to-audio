a2215713ba4a513d2dbc182f5f4259b3f62eed16
kayros-paradox <vugi915@gmail.com>
2025-07-22 22:08:03 +0500

    delete two .py files

--- pip-sources.sh
+++ pip-sources.sh
@@ -1,9 +1,6 @@
 #!/bin/bash/
 pip3 install --upgrade moviepy
 pip3 install --upgrade questionary
-pip3 uninstall SpeechRecognition 
-pip3 uninstall vosk
 pip3 install --upgrade PyAudio
 pip3 install --upgrade mutagen
-pip3 uninstall transformers
 
