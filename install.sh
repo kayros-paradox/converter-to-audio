d5ae33767d7094428df6a3e893691c0480bbd8a5
kayros-paradox <vugi915@gmail.com>
2025-07-23 15:18:25 +0500

    edit scripts

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# Install packages.
 pip3 install --upgrade moviepy
 pip3 install --upgrade questionary
 pip3 install --upgrade PyAudio
 pip3 install --upgrade mutagen
 pip3 install --upgrade pyinstaller
+# Create bin files.
 pyinstaller --onefile videotomp3.py
 pyinstaller --onefile videotowav.py
 pyinstaller --onefile videotoogg.py
+# Move bin files.
 mv dist/videoto* .
+# Delete build files.
 rm -rf build dist *.spec
--- pip-sources.sh
+++ pip-sources.sh
@@ -1,4 +1,5 @@
 #!/bin/bash/
+# Install pip packages.
 pip3 install --upgrade moviepy
 pip3 install --upgrade questionary
 pip3 install --upgrade PyAudio
